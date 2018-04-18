369587a1c64c844c8021536ea7f52558eb62b676
Paul Trampert <paul.trampert@gmail.com>
2018-04-18 15:57:52 -0400

    Kill missing _start warning

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,4 +1,6 @@
+global _start
 [bits 32]
 [extern kernel_main] ; Define the calling point. Must have the same name as kernel.c 'main' function
+_start:
 call kernel_main ; Calls the C function. The linker will know where it is placed in memory
 jmp $
